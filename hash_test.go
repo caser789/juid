7e7b9123ff3e2f300e6bf42e1c8303072044f26c
Xue Jiao <jiao.xue@seamoney.com>
2021-11-13 19:57:37 +0800

    impl NewMD5 NewSHA1

---
+++ hash_test.go
@@ -0,0 +1,21 @@
+package uuid
+
+import (
+	"testing"
+)
+
+func TestMD5(t *testing.T) {
+	uuid := NewMD5(NameSpace_DNS, []byte("python.org")).String()
+	want := "6fa459ea-ee8a-3ca4-894e-db77e160355e"
+	if uuid != want {
+		t.Errorf("MD5: got %q expected %q\n", uuid, want)
+	}
+}
+
+func TestSHA1(t *testing.T) {
+	uuid := NewSHA1(NameSpace_DNS, []byte("python.org")).String()
+	want := "886313e1-3b8a-5372-9b90-0c9aee199e5d"
+	if uuid != want {
+		t.Errorf("SHA1: got %q expected %q\n", uuid, want)
+	}
+}
