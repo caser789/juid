4f96650807cba7a5361d16f550c4490da7a6f8e7
Xue Jiao <jiao.xue@seamoney.com>
2021-11-15 11:17:36 +0800

    NewRandom -> New

--- version4_test.go
+++ version4_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestVersion4NewRandom(t *testing.T) {
 	for i := 0; i < 1000; i++ {
-		u := NewRandom()
+		u := New()
 
 		assert.Equal(t, u[6]&0b11110000, uint8(0x46)&0b11110000)
 		assert.Equal(t, u[8]&0b11000000, uint8(0x8e)&0b11000000)
